834863192d315feec2f67a5d89317fae67d5f91a
Thomas Wilmering <thomas.wilmering@eccenca.com>
2025-04-17 13:43:12 +0100

    use pymupdf

--- cmem_plugin_pdf_extract/pdf_extract.py
+++ cmem_plugin_pdf_extract/pdf_extract.py
@@ -1,37 +1,16 @@
-"""Random values workflow plugin module
-
-Remove this and other example files after bootstrapping your project.
-"""
+"""Extract text from PDF files"""
 
 import re
 from collections.abc import Sequence
-from pathlib import Path
 
-import PyPDF2
-from cmem.cmempy.workspace.projects.resources import get_all_resources
+from cmem.cmempy.workspace.projects.resources import get_resources
 from cmem_plugin_base.dataintegration.context import ExecutionContext
 from cmem_plugin_base.dataintegration.description import Icon, Plugin, PluginParameter
-from cmem_plugin_base.dataintegration.entity import (
-    Entities,
-    Entity,
-    EntityPath,
-    EntitySchema,
-)
+from cmem_plugin_base.dataintegration.entity import Entities, Entity, EntityPath, EntitySchema
 from cmem_plugin_base.dataintegration.plugins import WorkflowPlugin
 from cmem_plugin_base.dataintegration.types import StringParameterType
 from cmem_plugin_base.dataintegration.utils import setup_cmempy_user_access
-
-
-def extract_pdf_text(pdf_path: str) -> str:
-    """Extrahiert den Text aus einer PDF-Datei."""
-    text = ""
-    with Path(pdf_path).open("rb") as file:
-        reader = PyPDF2.PdfReader(file)
-        for page in reader.pages:
-            page_text = page.extract_text()
-            if page_text:
-                text += page_text
-    return text
+from pymupdf import open as pymupdf_open
 
 
 @Plugin(
@@ -54,39 +33,28 @@ class PdfExtract(WorkflowPlugin):
     def __init__(self, regex: str) -> None:
         self.regex = regex
 
-    def get_entities(self, resources: list) -> Entities:
+    def get_entities(self, filenames: list) -> Entities:
         """Extract text from PDF"""
         entities = []
-
-        paths = [
-            EntityPath(path="filename"),
-            EntityPath(path="text"),
-        ]
-
-        schema = EntitySchema(type_uri="urn:row", paths=paths)
-
-        for i, resource in enumerate(resources):
-            filename = resource["name"]
+        paths = [EntityPath(path="filename"), EntityPath(path="text")]
+        schema = EntitySchema(type_uri="urn:entity", paths=paths)
+        for i, filename in enumerate(filenames):
             self.log.info(f"processing file {filename}")
-            pdf_file = (
-                Path("/data/datalake") / self.context.task.project_id() / "resources" / filename
-            )
-            text = extract_pdf_text(pdf_file)
-            values = [
-                [filename],
-                [text],
-            ]
+            pdf_path = f"/data/datalake/{self.context.task.project_id()}/resources/{filename}"
+            pdf_file = pymupdf_open(pdf_path)
+            text_list = [page.get_text() for page in pdf_file]
+            text = "\n".join(text_list)
+            values = [[filename], [text]]
             entities.append(Entity(uri=f"urn:{i + 1}", values=values))
         return Entities(entities=entities, schema=schema)
 
-    def execute(
-        self,
-        inputs: Sequence[Entities],  # noqa: ARG002
-        context: ExecutionContext,
-    ) -> Entities:
+    def execute(self, inputs: Sequence[Entities], context: ExecutionContext) -> Entities:  # noqa: ARG002
         """Run the workflow operator."""
         self.context = context
         setup_cmempy_user_access(context.user)
-        resources = [r for r in get_all_resources() if re.match(rf"{self.regex}", r["name"])]
-
-        return self.get_entities(resources)
+        filenames = [
+            r["name"]
+            for r in get_resources(context.task.project_id())
+            if re.match(rf"{self.regex}", r["name"])
+        ]
+        return self.get_entities(filenames)
