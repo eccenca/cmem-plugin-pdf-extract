cfe831ee340b52161a22a3b6f710fa5567ae8d22
Thomas Wilmering <thomas.wilmering@eccenca.com>
2025-05-07 09:59:51 -0600

    test stderr fix

--- tests/test_pdf_extract.py
+++ tests/test_pdf_extract.py
@@ -217,28 +217,28 @@ def setup_page_selection() -> Generator:
 #
 
 
-# @pytest.mark.usefixtures("setup_corrupted")
-# def test_invalid_pdf_2() -> None:
-#     """Test with corrupted pdf"""
-#     filename = f"{UUID4}_corrupted_2.pdf"
-#     entities = PdfExtract(
+@pytest.mark.usefixtures("setup_corrupted")
+def test_invalid_pdf_2() -> None:
+    """Test with corrupted pdf"""
+    filename = f"{UUID4}_corrupted_2.pdf"
+    entities = PdfExtract(
+        regex=filename,
+        table_strategy="lines",
+        error_handling="raise_on_error",
+    ).execute(inputs=[], context=TestExecutionContext(PROJECT_ID))
+
+    assert literal_eval(entities.entities[0].values[0][0]) == FILE_CORRUPTED_RESULT_2
+
+# with pytest.raises(
+#     ValueError,
+#     match=f"File {filename}, page 1: Text extraction error: Data-loss while decompressing "
+#     f"corrupted data",
+# ):
+#     PdfExtract(
 #         regex=filename,
 #         table_strategy="lines",
-#         error_handling="raise_on_error",
+#         error_handling="raise_on_error_and_warning",
 #     ).execute(inputs=[], context=TestExecutionContext(PROJECT_ID))
-#
-#     assert literal_eval(entities.entities[0].values[0][0]) == FILE_CORRUPTED_RESULT_2
-
-    # with pytest.raises(
-    #     ValueError,
-    #     match=f"File {filename}, page 1: Text extraction error: Data-loss while decompressing "
-    #     f"corrupted data",
-    # ):
-    #     PdfExtract(
-    #         regex=filename,
-    #         table_strategy="lines",
-    #         error_handling="raise_on_error_and_warning",
-    #     ).execute(inputs=[], context=TestExecutionContext(PROJECT_ID))
 
 
 #
