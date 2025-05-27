9133fd5a70b8259d5495bdf72483619ae340fe56
Louis Westerheide <louis.westerheide@eccenca.com>
2025-05-27 09:19:33 +0200

    Remove default table extraction

--- cmem_plugin_pdf_extract/extraction_strategies/table_extraction_strategies.py
+++ cmem_plugin_pdf_extract/extraction_strategies/table_extraction_strategies.py
@@ -145,13 +145,3 @@ TABLE_EXTRACTION_STRATEGIES = {
     "lattice": LATTICE_STRATEGY,
     "sparse": SPARSE_STRATEGY,
 }
-
-CUSTOM_TABLE_STRATEGY_DEFAULT = """# vertical_strategy: lines
-# horizontal_strategy: lines
-# intersection_tolerance: 5
-# snap_tolerance: 3
-# join_tolerance: 3
-# edge_min_length: 3
-# min_words_vertical: 1
-# min_words_horizontal: 1
-"""
